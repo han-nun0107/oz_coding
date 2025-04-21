c909819dfa8812a9a9ada0e84b8a87e3ac111dbd
han-nun <spshspshek@gmail.com>
2025-04-21 15:20:34 +0900

    count별 글자색 변경

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <main className="flex flex-col justify-center items-center h-[100%] ">
+      <main className="flex flex-col justify-center items-center h-[100%]">
         <div
           className={twMerge(
             "text-amber-900",
